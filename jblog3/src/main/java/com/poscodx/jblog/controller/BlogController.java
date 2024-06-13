c98dd3e554ef0d029c12de7f9ae94562ea679d03
CHOI HyoJun <cjfgh2449@gmail.com>
2024-06-13 19:16:05 +0900

    feat. jblog 기본 틀 생성

---
+++ jblog3/src/main/java/com/poscodx/jblog/controller/BlogController.java
@@ -0,0 +1,39 @@
+package com.poscodx.jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/{id:(?!assets).*}")
+public class BlogController {
+	
+	@RequestMapping({"", "/{categoryNo}", "/{categoryNo}/{postNo}" })
+	public String index(
+		@PathVariable("id") String id,
+		@PathVariable("categoryNo") Long categoryNo,
+		@PathVariable("postNo") Long postNo) {
+
+		return "blog/main";
+	}
+	
+	// @Auth
+	@RequestMapping("/admin/basic")
+	public String adminBasic(@PathVariable("id") String id) {
+		return "blog/admin-basic";
+	}
+
+	// @Auth
+	@RequestMapping("/admin/category")
+	public String adminCategory(@PathVariable("id") String id) {
+		
+		return "blog/admin-category";
+	}
+	
+	//@Auth
+	@RequestMapping("/admin/write")
+	public String adminWrite(@PathVariable("id") String id) {
+		return "blog/admin-write";
+	}
+
+}
